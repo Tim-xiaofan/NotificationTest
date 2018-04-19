3beed0409d0c030a21ebe309a170333febe6fed2
Tim-xiaofan <34571885+Tim-xiaofan@users.noreply.github.com>
2018-04-19 19:50:51 +0800

    image

--- app/src/main/java/com/example/zhong/notificationtest/MainActivity.java
+++ app/src/main/java/com/example/zhong/notificationtest/MainActivity.java
@@ -2,12 +2,18 @@ package com.example.zhong.notificationtest;
 
 import android.app.Notification;
 import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Intent;
 import android.graphics.BitmapFactory;
+import android.graphics.Color;
+import android.net.Uri;
 import android.support.v4.app.NotificationCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+
+import java.io.File;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener{
 
@@ -23,6 +29,9 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     public void onClick(View v) {
         switch(v.getId()){
             case R.id.send_notice:
+                Intent intent = new Intent(this,NotificationActivity.class);
+                PendingIntent pendingIntent = PendingIntent.
+                        getActivity(this,0,intent,0);
                 NotificationManager manager = (NotificationManager)
                         getSystemService(NOTIFICATION_SERVICE);
                 Notification notification = new NotificationCompat.Builder(this)
@@ -31,6 +40,19 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                         .setWhen(System.currentTimeMillis())
                         .setSmallIcon(R.mipmap.ic_launcher)
                         .setLargeIcon(BitmapFactory.decodeResource(getResources(), R.mipmap.ic_launcher))
+                        .setContentIntent(pendingIntent)
+                        //.setAutoCancel(true)
+                        .setSound(Uri.fromFile(new File("system/media/audio/ringtones/Luna.ogg")))
+                        //.setVibrate(new long[]{0,1000,1000,1000})
+                        //.setLights(Color.GREEN,1000,100)
+                        //.setDefaults(NotificationCompat.DEFAULT_ALL)
+                        /*.setStyle(new NotificationCompat.BigTextStyle().bigText("Learn how to build" +
+                                " notifications, send and sync data, and use voice actions. Get the" +
+                                " official Android IDE and developer tools to build apps" +
+                                " for Android."))*/
+                        .setStyle(new NotificationCompat.BigPictureStyle().bigPicture(BitmapFactory.
+                                decodeResource(getResources(),R.drawable.big_image)))
+                        .setPriority(NotificationCompat.PRIORITY_MAX)
                         .build();
                 manager.notify(1, notification);
                 break;
